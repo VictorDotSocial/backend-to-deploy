498b2d26264745f3586dcbf072a4c006ca0e6320
Víctor <victor@MacBook-Pro-de-Victor.local>
2023-02-26 18:57:34 +0100

    Minor fix

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get("/test", (req, res) => {
   res.json({
