aee18167bb93a9d1ab122c001e974ade58dffc57
Víctor <victor@MacBook-Pro-de-Victor.local>
2023-02-26 19:15:46 +0100

    Minor fix

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ app.get("/test", (req, res) => {
   });
 });
 
-app.listen(process.env.APP_PORT || 3000, () => {
+app.listen(process.env.PORT || `0.0.0.0:$PORT`, () => {
   console.log("Servidor rulando");
 });
